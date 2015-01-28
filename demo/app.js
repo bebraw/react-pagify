2a1a74ba2755a796981c4c2aaa61a8ee85e909f7
Juho Vepsalainen <bebraw@gmail.com>
2015-01-28 15:40:19 +0200

    Add fork ribbon to demo

--- demo/app.js
+++ demo/app.js
@@ -1,10 +1,12 @@
 'use strict';
 
 require('highlight.js/styles/github.css');
+require('react-ghfork/style.css');
 require('./demo.css');
 require('../style.css');
 
 var React = require('react');
+var Fork = require('react-ghfork');
 
 var Paginate = require('../index.jsx');
 
@@ -14,6 +16,7 @@ var readme = require('../README.md');
 module.exports = React.createClass({
     render() {
         return <article>
+            <Fork project='bebraw/react-pagify'></Fork>
             <Paginate></Paginate>
             <div dangerouslySetInnerHTML={{__html: readme}}></div>
         </article>;
