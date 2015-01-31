8fc672ef70992f03d903d13e57315b3b1567f6af
Juho Vepsalainen <bebraw@gmail.com>
2015-01-31 21:53:16 +0200

    Update dist

--- dist/react-pagify.js
+++ dist/react-pagify.js
@@ -58,42 +58,37 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var React = __webpack_require__(1);
 
+	var segmentize = __webpack_require__(2);
+
 
 	var Paginator = React.createClass({displayName: "Paginator",
 	    render:function() {
 	        var onSelect = this.props.onSelect || noop;
 	        var page = this.props.page;
-	        var pages = this.props.pages;
+
+	        var segments = segmentize(this.props);
+	        segments = segments.reduce(function(a, b) {
+	            return a.concat(-1).concat(b);
+	        });
 
 	        return React.createElement("ul", {className: "pagination"}, 
-	            range(pages).map(function(i) 
-	                {return React.createElement("li", {
+	            segments.map(function(num, i) 
+	                {return num >= 0? React.createElement("li", {
 	                    key: 'pagination-' + i, 
-	                    onClick: onSelect.bind(null, i), 
-	                    className: i === page && 'selected'}, 
+	                    onClick: onSelect.bind(null, num), 
+	                    className: num === page && 'selected'}, 
 	                    React.createElement("a", {href: "#", onClick: this.preventDefault}, 
-	                        i + 1
+	                        num + 1
 	                    )
-	                );}.bind(this)
+	                ): React.createElement("li", {key: 'pagination-' + i}, "…");}.bind(this)
 	            )
-	        );
+	        )
 	    },
 
 	    preventDefault:function(e) {
 	        e.preventDefault();
 	    },
 	});
-
-	function range(amount) {
-	    var ret = [];
-	    var i;
-
-	    for(i = 0; i < amount; i++) {
-	        ret.push(i);
-	    }
-
-	    return ret;
-	}
 
 	function paginate(data, o) {
 	    data = data || [];
@@ -123,6 +118,168 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports, __webpack_require__) {
 
 	module.exports = __WEBPACK_EXTERNAL_MODULE_1__;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var intersect = __webpack_require__(4);
+	var uniq = __webpack_require__(5);
+
+	var range = __webpack_require__(3);
+
+
+	module.exports = function(o) {
+	    var page = o.page;
+	    var pages = o.pages;
+	    var beginPages = o.beginPages? range(o.beginPages): [];
+	    var endPages = o.endPages? range(pages - o.endPages, pages): [];
+	    var center;
+
+	    if(page === 0) {
+	        return (pages > 1? [[0, 1], endPages]: [[0]]).filter(function(a)  {return a.length;});
+	    }
+
+	    if(page === pages - 1) {
+	        return [beginPages, [pages - 2, pages - 1]].filter(function(a)  {return a.length;});
+	    }
+
+	    center = [page - 1, page, page + 1];
+
+	    if(intersect(beginPages, center).length) {
+	        beginPages = uniq(beginPages.concat(center));
+	        center = [];
+	    }
+
+	    if(intersect(center, endPages).length) {
+	        endPages = uniq(center.concat(endPages));
+	        center = [];
+	    }
+
+	    return[beginPages, center, endPages].filter(function(a)  {return a.length;});
+	};
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+
+	module.exports = function(a, b) {
+	    var ret = [];
+	    var i = b? a: 0;
+	    var len = b? b: a;
+
+	    for(; i < len; i++) {
+	        ret.push(i);
+	    }
+
+	    return ret;
+	};
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = intersect;
+
+	function intersect (a, b) {
+	  var res = [];
+	  for (var i = 0; i < a.length; i++) {
+	    if (indexOf(b, a[i]) > -1) res.push(a[i]);
+	  }
+	  return res;
+	}
+
+	intersect.big = function(a, b) {
+	  var ret = [];
+	  var temp = {};
+	  
+	  for (var i = 0; i < b.length; i++) {
+	    temp[b[i]] = true;
+	  }
+	  for (var i = 0; i < a.length; i++) {
+	    if (temp[a[i]]) ret.push(a[i]);
+	  }
+	  
+	  return ret;
+	}
+
+	function indexOf(arr, el) {
+	  for (var i = 0; i < arr.length; i++) {
+	    if (arr[i] === el) return i;
+	  }
+	  return -1;
+	}
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict"
+
+	function unique_pred(list, compare) {
+	  var ptr = 1
+	    , len = list.length
+	    , a=list[0], b=list[0]
+	  for(var i=1; i<len; ++i) {
+	    b = a
+	    a = list[i]
+	    if(compare(a, b)) {
+	      if(i === ptr) {
+	        ptr++
+	        continue
+	      }
+	      list[ptr++] = a
+	    }
+	  }
+	  list.length = ptr
+	  return list
+	}
+
+	function unique_eq(list) {
+	  var ptr = 1
+	    , len = list.length
+	    , a=list[0], b = list[0]
+	  for(var i=1; i<len; ++i, b=a) {
+	    b = a
+	    a = list[i]
+	    if(a !== b) {
+	      if(i === ptr) {
+	        ptr++
+	        continue
+	      }
+	      list[ptr++] = a
+	    }
+	  }
+	  list.length = ptr
+	  return list
+	}
+
+	function unique(list, compare, sorted) {
+	  if(list.length === 0) {
+	    return list
+	  }
+	  if(compare) {
+	    if(!sorted) {
+	      list.sort(compare)
+	    }
+	    return unique_pred(list, compare)
+	  }
+	  if(!sorted) {
+	    list.sort()
+	  }
+	  return unique_eq(list)
+	}
+
+	module.exports = unique
+
 
 /***/ }
 /******/ ])
