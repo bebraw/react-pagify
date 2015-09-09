508fcc82400bf7688636249edc4a4c1861fd6092
Juho Vepsalainen <bebraw@gmail.com>
2015-09-09 16:13:55 +0300

    Update externals so that global entry point works in UMD

--- config/webpack.common.dist.js
+++ config/webpack.common.dist.js
@@ -7,7 +7,12 @@ var common = require('./webpack.common');
 module.exports = extend(common, {
     entry: './lib/index',
     externals: {
-        react: 'react',
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'React',
+            root: 'React'
+        }
     },
     module: {
         loaders: common.loaders.concat([{
