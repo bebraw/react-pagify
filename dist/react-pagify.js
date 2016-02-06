9eaec3148f340dbe7d84db94c12c4617fcaeb769
Juho Vepsalainen <bebraw@gmail.com>
2016-02-06 22:07:12 +0200

    Update dist

--- dist/react-pagify.js
+++ dist/react-pagify.js
@@ -11,41 +11,41 @@
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
 /******/ 		if(installedModules[moduleId])
 /******/ 			return installedModules[moduleId].exports;
-
+/******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			exports: {},
 /******/ 			id: moduleId,
 /******/ 			loaded: false
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
-
+/******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
 /******/ })
@@ -55,161 +55,175 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
-	var React = __webpack_require__(1);
-	var segmentize = __webpack_require__(2);
-
-	var np = __webpack_require__(5);
-
-	var Paginator = React.createClass({
-	    displayName: 'Paginator',
-
-	    propTypes: {
-	        onSelect: React.PropTypes.func,
-	        page: React.PropTypes.number,
-	        beginPages: React.PropTypes.number,
-	        endPages: React.PropTypes.number,
-	        showPrevNext: React.PropTypes.bool,
-	        alwaysShowPrevNext: React.PropTypes.bool,
-	        className: React.PropTypes.string,
-	        ellipsesClassName: React.PropTypes.string,
-	        prevClassName: React.PropTypes.string,
-	        nextClassName: React.PropTypes.string,
-	        activeClassName: React.PropTypes.string,
-	        inactiveClassName: React.PropTypes.string,
-	        prevButton: React.PropTypes.node,
-	        nextButton: React.PropTypes.node,
-	        ellipsisButton: React.PropTypes.node
-	    },
-	    getDefaultProps: function getDefaultProps() {
-	        return {
-	            onSelect: noop,
-	            showPrevNext: false,
-	            className: 'pagify-pagination',
-	            ellipsesClassName: '',
-	            prevClassName: 'pagify-prev',
-	            nextClassName: 'pagify-next',
-	            activeClassName: 'selected',
-	            inactiveClassName: 'pagify-disabled',
-	            ellipsisButton: '…'
-	        };
-	    },
-	    render: function render() {
-	        var _this = this;
-
-	        var _props = this.props;
-	        var onSelect = _props.onSelect;
-	        var page = _props.page;
-	        var ellipsesClassName = _props.ellipsesClassName;
-	        var className = _props.className;
-	        var showPrevNext = _props.showPrevNext;
-	        var alwaysShowPrevNext = _props.alwaysShowPrevNext;
-	        var prevClassName = _props.prevClassName;
-	        var nextClassName = _props.nextClassName;
-	        var activeClassName = _props.activeClassName;
-	        var inactiveClassName = _props.inactiveClassName;
-	        var ellipsisButton = _props.ellipsisButton;
-
-	        var segments = segmentize(this.props);
-	        segments = segments.reduce(function (a, b) {
-	            return a.concat(-1).concat(b);
-	        });
-
-	        var items = segments.map(function (num, i) {
-	            if (num >= 0) {
-	                return React.createElement(
-	                    'li',
-	                    {
-	                        key: 'pagination-' + i,
-	                        onClick: onSelect.bind(null, num),
-	                        className: num === page && activeClassName || ''
-	                    },
-	                    React.createElement(
-	                        'a',
-	                        { href: '#', onClick: _this.preventDefault },
-	                        num + 1
-	                    )
-	                );
-	            }
-
-	            return React.createElement(
-	                'li',
-	                {
-	                    key: 'pagination-' + i,
-	                    className: ellipsesClassName
-	                },
-	                ellipsisButton
-	            );
-	        });
-
-	        var lastPage = segments[segments.length - 1];
-
-	        var isFirstPage = page === 0;
-	        var isLastPage = page === lastPage;
-
-	        prevClassName += np.maybeAddInactive(isFirstPage, alwaysShowPrevNext, inactiveClassName);
-	        nextClassName += np.maybeAddInactive(isLastPage, alwaysShowPrevNext, inactiveClassName);
-
-	        var prevButton = React.createElement(
-	            'li',
-	            {
-	                onClick: onSelect.bind(null, np.prev(page)),
-	                className: prevClassName
-	            },
-	            React.createElement(
-	                'a',
-	                { href: '#', onClick: this.preventDefault },
-	                this.props.prevButton ? this.props.prevButton : 'Previous'
-	            )
-	        );
-
-	        var nextButton = React.createElement(
-	            'li',
-	            {
-	                onClick: onSelect.bind(null, np.next(page, lastPage)),
-	                className: nextClassName
-	            },
-	            React.createElement(
-	                'a',
-	                { href: '#', onClick: this.preventDefault },
-	                this.props.nextButton ? this.props.nextButton : 'Next'
-	            )
-	        );
-
-	        return React.createElement(
-	            'ul',
-	            { className: className },
-	            (alwaysShowPrevNext || showPrevNext && !isFirstPage) && prevButton,
-	            items,
-	            (alwaysShowPrevNext || showPrevNext && !isLastPage) && nextButton
-	        );
-	    },
-
-	    preventDefault: function preventDefault(e) {
-	        e.preventDefault();
-	    }
+	
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
 	});
-
-	function paginate(data, o) {
-	    data = data || [];
-
-	    var page = o.page || 0;
-	    var perPage = o.perPage;
-
-	    var amountOfPages = Math.ceil(data.length / perPage);
-	    var startPage = page < amountOfPages ? page : 0;
-
-	    return {
-	        amount: amountOfPages,
-	        data: data.slice(startPage * perPage, startPage * perPage + perPage),
-	        page: startPage
-	    };
-	}
-
-	function noop() {}
-
-	Paginator.paginate = paginate;
-
-	module.exports = Paginator;
+	
+	var _react = __webpack_require__(1);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var Context = function (_React$Component) {
+	  _inherits(Context, _React$Component);
+	
+	  function Context() {
+	    _classCallCheck(this, Context);
+	
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Context).apply(this, arguments));
+	  }
+	
+	  _createClass(Context, [{
+	    key: 'getChildContext',
+	    value: function getChildContext() {
+	      return {
+	        segments: this.props.segments,
+	        onSelect: this.props.onSelect
+	      };
+	    }
+	  }, {
+	    key: 'render',
+	    value: function render() {
+	      var _props = this.props;
+	      var onSelect = _props.onSelect;
+	      var segments = _props.segments;
+	
+	      var props = _objectWithoutProperties(_props, ['onSelect', 'segments']);
+	
+	      return _react2.default.createElement(
+	        'div',
+	        props,
+	        this.props.children
+	      );
+	    }
+	  }]);
+	
+	  return Context;
+	}(_react2.default.Component);
+	
+	Context.propTypes = {
+	  children: _react2.default.PropTypes.any,
+	  onSelect: _react2.default.PropTypes.func,
+	  segments: _react2.default.PropTypes.object
+	};
+	Context.childContextTypes = {
+	  onSelect: _react2.default.PropTypes.func,
+	  segments: _react2.default.PropTypes.object
+	};
+	
+	var Segment = function (_React$Component2) {
+	  _inherits(Segment, _React$Component2);
+	
+	  function Segment() {
+	    _classCallCheck(this, Segment);
+	
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Segment).apply(this, arguments));
+	  }
+	
+	  _createClass(Segment, [{
+	    key: 'render',
+	    value: function render() {
+	      var context = this.context;
+	      var props = this.props;
+	      var segments = context.segments;
+	      var onSelect = context.onSelect;
+	      var pages = segments[props.field];
+	
+	      return _react2.default.createElement(
+	        'div',
+	        null,
+	        pages.map(function (page) {
+	          return _react2.default.createElement(
+	            'span',
+	            _extends({}, props, {
+	              key: 'page-' + page,
+	              onClick: function onClick(e) {
+	                return onSelect(page, e);
+	              } }),
+	            page
+	          );
+	        })
+	      );
+	    }
+	  }]);
+	
+	  return Segment;
+	}(_react2.default.Component);
+	
+	Segment.propTypes = {
+	  field: _react2.default.PropTypes.string.isRequired
+	};
+	Segment.contextTypes = {
+	  onSelect: _react2.default.PropTypes.func,
+	  segments: _react2.default.PropTypes.object
+	};
+	
+	var Ellipsis = function (_React$Component3) {
+	  _inherits(Ellipsis, _React$Component3);
+	
+	  function Ellipsis() {
+	    _classCallCheck(this, Ellipsis);
+	
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Ellipsis).apply(this, arguments));
+	  }
+	
+	  _createClass(Ellipsis, [{
+	    key: 'render',
+	    value: function render() {
+	      var context = this.context;
+	      var props = this.props;
+	      var segments = context.segments;
+	      var children = props.children;
+	      var previousPages = segments[props.previousField];
+	      var nextPages = segments[props.nextField];
+	      var showEllipsis = nextPages[0] - previousPages.slice(-1)[0] > 1;
+	
+	      if (showEllipsis) {
+	        return _react2.default.createElement(
+	          'span',
+	          props,
+	          children
+	        );
+	      }
+	
+	      return null;
+	    }
+	  }]);
+	
+	  return Ellipsis;
+	}(_react2.default.Component);
+	
+	Ellipsis.propTypes = {
+	  children: _react2.default.PropTypes.any.isRequired,
+	  previousField: _react2.default.PropTypes.string.isRequired,
+	  nextField: _react2.default.PropTypes.string.isRequired
+	};
+	Ellipsis.defaultProps = {
+	  children: '…'
+	};
+	Ellipsis.contextTypes = {
+	  segments: _react2.default.PropTypes.object
+	};
+	
+	exports.default = {
+	  Context: Context,
+	  Segment: Segment,
+	  Ellipsis: Ellipsis
+	};
 
 /***/ },
 /* 1 */
@@ -217,276 +231,8 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	module.exports = __WEBPACK_EXTERNAL_MODULE_1__;
 
-/***/ },
-/* 2 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-	const intersect = __webpack_require__(3);
-	const uniq = __webpack_require__(4);
-
-	module.exports = function (o) {
-	  const page = o.page;
-	  const pages = o.pages;
-	  const leftSidePages = o.sidePages ?
-	    range(Math.max(page - o.sidePages, 0), page) :
-	    [page - 1];
-	  const rightSidePages = o.sidePages ?
-	    range(page + 1, Math.min(page + o.sidePages + 1, pages)) :
-	    [page + 1];
-	  var beginPages = o.beginPages ? range(Math.min(o.beginPages, pages)) : [];
-	  var endPages = o.endPages ? range(Math.max(pages - o.endPages, 0), pages) : [];
-	  var center;
-
-	  if (beginPages.length + endPages.length >= pages) {
-	    return [range(pages)];
-	  }
-
-	  if (page === 0) {
-	    if (pages > 1) {
-	      if (!beginPages.length) {
-	        beginPages = [0, 1];
-	      }
-	      beginPages = uniq(beginPages.concat(rightSidePages));
-
-	      return [beginPages, difference(endPages, beginPages)].filter(function (a) {
-	        return a.length;
-	      });
-	    }
-
-	    return [[0]];
-	  }
-
-	  if (page === pages - 1) {
-	    endPages = [pages - 2, pages - 1];
-	    endPages = uniq(endPages.concat(leftSidePages));
-
-	    return [beginPages, difference(endPages, beginPages)].filter(function (a) {
-	      return a.length;
-	    });
-	  }
-
-	  center = [].concat(leftSidePages, [page], rightSidePages);
-
-	  if (intersect(beginPages, center).length) {
-	    beginPages = uniq(beginPages.concat(center)).sort(function (a, b) {
-	      return a > b;
-	    });
-
-	    center = [];
-	  }
-
-	  if (intersect(center, endPages).length) {
-	    endPages = uniq(center.concat(endPages)).sort(function (a, b) {
-	      return a > b;
-	    });
-
-	    center = [];
-	  }
-
-	  if (!center.length &&
-	      beginPages.length === endPages.length &&
-	      beginPages.every(function (p, i) {
-	        return p === endPages[i];
-	      })) {
-	    return [beginPages];
-	  }
-
-	  if (!center.length && intersect(beginPages, endPages).length ||
-	      endPages[0] - beginPages.slice(-1)[0] === 1) {
-	    return [uniq(beginPages.concat(endPages)).sort(function (a, b) {
-	      return a > b;
-	    })];
-	  }
-
-	  if (center[0] - beginPages.slice(-1)[0] === 1) {
-	    return [beginPages.concat(center), endPages];
-	  }
-
-	  if (endPages[0] - center.slice(-1)[0] === 1) {
-	    return [beginPages, center.concat(endPages)];
-	  }
-
-	  return [beginPages, center, endPages].filter(function (a) {
-	    return a.length;
-	  });
-	};
-
-	function range(a, b) {
-	  const len = b ? b : a;
-	  const ret = [];
-	  var i = b ? a : 0;
-
-	  for (; i < len; i++) {
-	    ret.push(i);
-	  }
-
-	  return ret;
-	}
-
-	function difference(a, b) {
-	  return a.filter(function (v) {
-	    return b.indexOf(v) < 0;
-	  });
-	}
-
-
-/***/ },
-/* 3 */
-/***/ function(module, exports) {
-
-	module.exports = intersect;
-
-	function many (sets) {
-	  var o = {};
-	  var l = sets.length - 1;
-	  var first = sets[0];
-	  var last = sets[l];
-	  
-	  for(var i in first) o[first[i]] = 0;
-	  
-	  for(var i = 1; i <= l; i++) {
-	    var row = sets[i];
-	    for(var j in row) {
-	      var key = row[j];
-	      if(o[key] === i - 1) o[key] = i;
-	    }
-	  }
-	  
-	  var a = [];
-	  for(var i in last) {
-	    var key = last[i];
-	    if(o[key] === l) a.push(key);
-	  }
-	  
-	  return a;
-	}
-
-	function intersect (a, b) {
-	  if (!b) return many(a);
-
-	  var res = [];
-	  for (var i = 0; i < a.length; i++) {
-	    if (indexOf(b, a[i]) > -1) res.push(a[i]);
-	  }
-	  return res;
-	}
-
-	intersect.big = function(a, b) {
-	  if (!b) return many(a);
-	  
-	  var ret = [];
-	  var temp = {};
-	  
-	  for (var i = 0; i < b.length; i++) {
-	    temp[b[i]] = true;
-	  }
-	  for (var i = 0; i < a.length; i++) {
-	    if (temp[a[i]]) ret.push(a[i]);
-	  }
-	  
-	  return ret;
-	}
-
-	function indexOf(arr, el) {
-	  for (var i = 0; i < arr.length; i++) {
-	    if (arr[i] === el) return i;
-	  }
-	  return -1;
-	}
-
-
-/***/ },
-/* 4 */
-/***/ function(module, exports) {
-
-	"use strict"
-
-	function unique_pred(list, compare) {
-	  var ptr = 1
-	    , len = list.length
-	    , a=list[0], b=list[0]
-	  for(var i=1; i<len; ++i) {
-	    b = a
-	    a = list[i]
-	    if(compare(a, b)) {
-	      if(i === ptr) {
-	        ptr++
-	        continue
-	      }
-	      list[ptr++] = a
-	    }
-	  }
-	  list.length = ptr
-	  return list
-	}
-
-	function unique_eq(list) {
-	  var ptr = 1
-	    , len = list.length
-	    , a=list[0], b = list[0]
-	  for(var i=1; i<len; ++i, b=a) {
-	    b = a
-	    a = list[i]
-	    if(a !== b) {
-	      if(i === ptr) {
-	        ptr++
-	        continue
-	      }
-	      list[ptr++] = a
-	    }
-	  }
-	  list.length = ptr
-	  return list
-	}
-
-	function unique(list, compare, sorted) {
-	  if(list.length === 0) {
-	    return list
-	  }
-	  if(compare) {
-	    if(!sorted) {
-	      list.sort(compare)
-	    }
-	    return unique_pred(list, compare)
-	  }
-	  if(!sorted) {
-	    list.sort()
-	  }
-	  return unique_eq(list)
-	}
-
-	module.exports = unique
-
-
-/***/ },
-/* 5 */
-/***/ function(module, exports) {
-
-	'use strict';
-
-	module.exports = {
-	    // Increment page, unless it is >= lastPage, then return lastPage.
-	    next: function next(page, lastPage) {
-	        return page >= lastPage ? lastPage : page + 1;
-	    },
-
-	    // Decrement page, unless it is <= firstPage, then return firstPage.
-	    prev: function prev(page, firstPage) {
-	        firstPage = firstPage || 0;
-	        return page <= firstPage ? firstPage : page - 1;
-	    },
-
-	    // Return the inactiveClassName if it should be added.
-	    maybeAddInactive: function maybeAddInactive(isFirstOrLastPage, alwaysShowPrevNext, inactiveClassName) {
-	        if (isFirstOrLastPage && alwaysShowPrevNext && inactiveClassName) {
-	            return ' ' + inactiveClassName;
-	        }
-	        return '';
-	    }
-	};
-
 /***/ }
 /******/ ])
 });
 ;
+//# sourceMappingURL=react-pagify.js.map
